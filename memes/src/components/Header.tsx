c1fa18e6ed6da41d4c1d9e7a42af7443e7ccdded
DAngelo-Diego <dangelodiego967@gmail.com>
2022-08-17 12:10:38 -0300

    alt added to header

--- memes/src/components/Header.tsx
+++ memes/src/components/Header.tsx
@@ -3,7 +3,11 @@ import "../stylesheets/header.css";
 const Header = () => {
   return (
     <header className="header">
-      <img src="../../public/img/logo.png " className="header--image" />
+      <img
+        src="../../public/img/logo.png "
+        className="header--image"
+        alt="logo"
+      />
       <h2 className="header--title">Memes Generator</h2>
       <h4 className="header--project">Created by: "D'Angelo Diego"</h4>
     </header>

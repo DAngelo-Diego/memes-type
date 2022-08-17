30d0049b4bb89b7925e5b7a4580e4bd1901d3e11
DAngelo-Diego <dangelodiego967@gmail.com>
2022-08-17 09:39:55 -0300

    Memes-Generator

--- memes/src/components/Meme.tsx
+++ memes/src/components/Meme.tsx
@@ -1,20 +1,63 @@
 import "../stylesheets/meme.css";
 import memesData from "./memes.Data";
+import { useState } from "react";
 
 const Meme = () => {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setallMemeImages] = useState(memesData);
+
   const random = () => {
-    const memesArr = memesData.data.memes;
+    const memesArr = allMemeImages.data.memes;
     const randomNum = Math.floor(Math.random() * memesArr.length);
-    const { url } = memesArr[randomNum];
+    const url = memesArr[randomNum].url;
+    setMeme((meme) => ({
+      ...meme,
+      randomImage: url,
+    }));
   };
+
+  function handleChange(event: any) {
+    const { name, value } = event.target;
+    setMeme((meme) => ({
+      ...meme,
+      [name]: value,
+    }));
+  }
+
+  //direct to DOM
+
   return (
     <main>
       <div className="form">
-        <input type="text" className="form--input" placeholder="Top text" />
-        <input type="text" className="form--input" placeholder="Bottom text" />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Top text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Bottom text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className="form--button" onClick={random}>
           Get a new meme image
         </button>
+      </div>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
     </main>
   );

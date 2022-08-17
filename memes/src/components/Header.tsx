0e516a4d409ace5e332c9ade02f851cf4ba4ef4d
DAngelo-Diego <dangelodiego967@gmail.com>
2022-08-17 12:26:19 -0300

    header edit

--- memes/src/components/Header.tsx
+++ memes/src/components/Header.tsx
@@ -1,10 +1,8 @@
 import "../stylesheets/header.css";
-import logo from "../../public/img/logo.png";
 
 const Header = () => {
   return (
     <header className="header">
-      <img src={logo} className="header--image" alt="page-logo" />
       <h2 className="header--title">Memes Generator</h2>
       <h4 className="header--project">Created by: "D'Angelo Diego"</h4>
     </header>
